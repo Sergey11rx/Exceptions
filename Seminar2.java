e6f6572f240b7ed4947cadbc50c2498017b13ae5
Sergey <gyraserg@mail.ru>
2023-09-06 17:38:16 +0300

    first commit

--- Seminar2.java
+++ Seminar2.java
@@ -1,3 +1,6 @@
+// 1. Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает введенное значение. 
+// Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.
+
 import java.util.Scanner;
 
 public class Main {
@@ -24,5 +27,111 @@ public class Main {
         }
 
         return number;
+    }
+}
+
+
+
+// 2. Если необходимо, исправьте данный код:
+
+// try {
+//    int d = 0;
+//    double catchedRes1 = intArray[8] / d;
+//    System.out.println("catchedRes1 = " + catchedRes1);
+// } catch (ArithmeticException e) {
+//    System.out.println("Catching exception: " + e);
+// }
+
+// Решение:
+
+try {
+   int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+   int d = 0;
+   double catchedRes1 = intArray[8] / d;
+   System.out.println("catchedRes1 = " + catchedRes1);
+} catch (ArithmeticException e) {
+   System.out.println("Catching exception: " + e);
+}
+
+
+
+// 3. Дан следующий код, исправьте его там, где требуется:
+
+// public static void main(String[] args) throws Exception {
+//    try {
+//        int a = 90;
+//        int b = 3;
+//        System.out.println(a / b);
+//        printSum(23, 234);
+//        int[] abc = { 1, 2 };
+//        abc[3] = 9;
+//    } catch (Throwable ex) {
+//        System.out.println("Что-то пошло не так...");
+//    } catch (NullPointerException ex) {
+//        System.out.println("Указатель не может указывать на null!");
+//    } catch (IndexOutOfBoundsException ex) {
+//        System.out.println("Массив выходит за пределы своего размера!");
+//    }
+// }
+// public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+//    System.out.println(a + b);
+// }
+
+// Решение:
+
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[2] = 9;
+        } catch (ArithmeticException ex) {
+            System.out.println("Деление на ноль!");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Выход за пределы массива!");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Файл не найден!");
+        } catch (Exception ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+}
+
+
+
+// 4. Разработайте программу, которая выбросит Exception, 
+// когда пользователь вводит пустую строку. Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+
+import java.util.Scanner;
+
+public class EmptyStringExceptionExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String input = scanner.nextLine();
+
+        try {
+            if (input.isEmpty()) {
+                throw new Exception("Пустые строки вводить нельзя");
+            } else {
+                System.out.println("Вы ввели: " + input);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
